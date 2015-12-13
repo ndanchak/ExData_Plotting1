293edd6f22ad5abdb9fccbdd5c66dd11a9f1984b
ndanchak <nestor.danchak@gmail.com>
2015-12-13 19:20:14 +0200

    Added latest code for plot2

--- plot2.R
+++ plot2.R
@@ -1,13 +1,15 @@
+Sys.setlocale("LC_ALL", "English")
+
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
 data <- read.table(unz(temp, "household_power_consumption.txt"),header=TRUE,na.strings = "NA",sep = ";")
 unlink(temp)
 
 filtereddata <- data[(as.Date(data$Date, format = "%d/%m/%Y") == "2007-02-01" | as.Date(data$Date, format = "%d/%m/%Y") == "2007-02-02"),]
+filtereddata$Date <- as.Date(filtereddata$Date, format = "%d/%m/%Y")
 filtereddata$Global_active_power <- as.numeric(as.character(filtereddata$Global_active_power))
+filtereddata$DateTime <- with(filtereddata, as.POSIXct(paste(Date, Time), format="%Y-%m-%d %H:%M:%S", tz = "GMT"))
 
-png(filename="plot1.png")
-hist(filtereddata$Global_active_power
-     ,xlab="Global Active Power (kilowatts)",col = "red",main="Global Active Power",breaks = 12)
+png(filename="plot2.png")
+with(filtereddata,plot(DateTime,Global_active_power, type = "l", ylab = "Global Active Power (kilowatts)", xlab =" "))
 dev.off()
-
